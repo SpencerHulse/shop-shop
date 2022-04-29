a2c3db08775d33c9998738986e8c3f9940e29d3b
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-28 20:52:32 -0400

    Created a custom hook to hold global state using Context.

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,32 @@
+// createContext creates the context container to hold the global state data and functionality
+// useContext is a hook that lets you to use the state created by createContext
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from "./reducers";
+
+const StoreContext = createContext();
+// All context objects have two components - Provider and Consumer
+// Provider: Wraps the application to make state data available to all other components
+// Consumer: Grabbing and using state from the Provider
+const { Provider } = StoreContext;
+
+// Instantiate the global state using useProductReducer
+// Because it (useProductReducer) wraps around the useReducer Hook from React, running it returns two things:
+// state: most up-to-date version of the global state object
+// dispatch: method to update state. Looks for an action object passed in as an argument.
+// The props are any additional props needed, which can add flexibility
+const StoreProvider = ({ value = [], ...props }) => {
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: "",
+  });
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+// Used by components that need the data provided by the <StoreProvider>
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
